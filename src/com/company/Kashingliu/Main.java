29153c7826c977d4d0a791e9c14ab3a7b5887bb0
KashingLiu <624453893@qq.com>
2017-11-30 22:10:30 +0800

    ~ 第一个是tf那个listener没有确定 第二个是如果在tf中输入多了字母，会查不到单词

--- src/com/company/Kashingliu/Main.java
+++ src/com/company/Kashingliu/Main.java
@@ -13,9 +13,9 @@ import java.util.Vector;
 public class Main  {
     private static String[] change_sharp(String a) {
         if (a!=null){
-            int second_split = a.lastIndexOf("#");
-            String re = a.substring(0,second_split);
-            return re.split("#");
+            String[] result = a.split("#");
+            String[] fine = {result[0],result[1]};
+            return fine;
         }
         else return null;
     }
@@ -31,7 +31,6 @@ public class Main  {
         }
         else return null;
     }
-
     private static String[] change_sharp_list(String a) {
         if (a!=null) {
             return a.split("#");
@@ -46,6 +45,7 @@ public class Main  {
         }
         else return null;
     }
+
     private static Vector out(String[] a) {
         Vector<String> out = new Vector<>(2);
         out.add(a[0]);
@@ -57,6 +57,16 @@ public class Main  {
         JFrame Main = new JFrame("天天背单词");
         Container ct = Main.getContentPane();
         ct.setLayout(new BoxLayout(ct,BoxLayout.X_AXIS));
+
+
+        // 把单词放入dic_list
+        ArrayList<String[]> dic_list = new ArrayList<>();
+        // 搜索结果以表格（JTable）实现，这个是列名
+        Vector<String> name = new Vector<>(2);
+        name.add("English");
+        name.add("中文");
+        // 表格内部模型
+        DefaultTableModel tableModel = new DefaultTableModel(name,100);
 
         String path = "Dict";
         FileReader all_dictionary_list = new FileReader("Dict/grouplist.txt");
@@ -89,11 +99,27 @@ public class Main  {
 //                super.mouseClicked(e);
                 JList theList = (JList) e.getSource();
                 if (e.getClickCount()==2) {
+                    dic_list.clear();
+                    tableModel.setRowCount(0);
                     int index = theList.locationToIndex(e.getPoint());
                     if (index >= 0) {
                         Object o = theList.getModel().getElementAt(index);
-//                        System.out.println(e.getPoint());
+                        try {
+                            String path = "Dict/"+map.get(o.toString());
+                            FileReader a = new FileReader(path);
+                            BufferedReader br = new BufferedReader(a);
+                            // 读取文件，对文件进行按行划分，用change_sharp来表示中间是#的，用**_is来表示中间是等号的
+                            String string = "";
+                            while ( string != null ) {
+                                string = br.readLine();
+                                if (string!=null) {
+                                    dic_list.add(change_sharp(string));
+                                    tableModel.addRow(change_sharp(string));
+                                }
+                            }
+                        } catch (Exception exc) {
 
+                        }
                     }
                 }
             }
@@ -102,16 +128,10 @@ public class Main  {
 
         // 放置搜索框和搜索结果的main JPanel
         JPanel main = new JPanel();
-        // 把单词放入dic_list
-        ArrayList<String[]> dic_list = new ArrayList<>();
+
         // 搜索框
         java.awt.TextField tf = new java.awt.TextField();
-        // 搜索结果以表格（JTable）实现，这个是列名
-        Vector<String> name = new Vector<>(2);
-        name.add("English");
-        name.add("中文");
-        // 表格内部模型
-        DefaultTableModel tableModel = new DefaultTableModel(name,100);
+
         JTable test_table = new JTable(tableModel);
         // 表格的下拉条
         JScrollPane jScrollPane = new JScrollPane(test_table);
@@ -134,18 +154,18 @@ public class Main  {
         // 搜索栏的最大尺寸和字体
         tf.setMaximumSize(new Dimension(200,30));
         tf.setFont(new Font("Menu.font",Font.PLAIN,15));
-        // 读文件的
-        FileReader a = new FileReader("/Users/Kashingliu/Documents/EnglishDictionary/Dict/College_Grade6.txt");
-        BufferedReader br = new BufferedReader(a);
-        // 读取文件，对文件进行按行划分，用change_sharp来表示中间是#的，用**_is来表示中间是等号的
-        String string = "";
-        while ( string != null ) {
-            string = br.readLine();
-            if (string!=null) {
-                dic_list.add(change_sharp(string));
-                tableModel.addRow(change_sharp(string));
-            }
-        }
+//        // 读文件的
+//        FileReader a = new FileReader("/Users/Kashingliu/Documents/EnglishDictionary/Dict/College_Grade6.txt");
+//        BufferedReader br = new BufferedReader(a);
+//        // 读取文件，对文件进行按行划分，用change_sharp来表示中间是#的，用**_is来表示中间是等号的
+//        String string = "";
+//        while ( string != null ) {
+//            string = br.readLine();
+//            if (string!=null) {
+//                dic_list.add(change_sharp(string));
+//                tableModel.addRow(change_sharp(string));
+//            }
+//        }
 
         // main使用BoxLayout，纵方向
         main.setLayout(new BoxLayout(main,BoxLayout.Y_AXIS));
